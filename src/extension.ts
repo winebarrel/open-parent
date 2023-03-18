927ba07cb728c5f5468bb87ef64457efccef6fdf
winebarrel <sugawara@winebarrel.jp>
2023-03-18 13:02:37 +0900

    implement feature

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,38 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+function openParentWorkspace(uri: vscode.Uri, forceNewWindow: boolean) {
+  const wsFolder = vscode.workspace.getWorkspaceFolder(uri);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "open-parent" is now active!');
+  if (!wsFolder) {
+    return;
+  }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('open-parent.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from open-parent!');
-	});
-
-	context.subscriptions.push(disposable);
+  const parent = vscode.Uri.joinPath(wsFolder.uri, "..");
+  vscode.commands.executeCommand("vscode.openFolder", parent, forceNewWindow);
 }
 
-// This method is called when your extension is deactivated
+export function activate(context: vscode.ExtensionContext) {
+  let reopenParentDirInNewWindow = vscode.commands.registerCommand(
+    "open-parent.reopenParentWorkspace",
+    (uri: any) => {
+      if (uri instanceof vscode.Uri) {
+        openParentWorkspace(uri, false);
+      }
+    }
+  );
+
+  context.subscriptions.push(reopenParentDirInNewWindow);
+
+  let openParentDirInNewWindow = vscode.commands.registerCommand(
+    "open-parent.openParentWorkspace",
+    (uri: any) => {
+      if (uri instanceof vscode.Uri) {
+        openParentWorkspace(uri, true);
+      }
+    }
+  );
+
+  context.subscriptions.push(openParentDirInNewWindow);
+}
+
 export function deactivate() {}
